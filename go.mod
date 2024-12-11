69887a4a71b1d3285de7ff706a0076dcf1ac82a2
OrbitalCodeRock <37189245+OrbitalCodeRock@users.noreply.github.com>
2024-12-11 17:56:13 -0600

    Setup initial project and dependencies

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/OrbitalCodeRock/simple_go_project_DB
+
+go 1.23.4
---
+++ main.go
@@ -0,0 +1,15 @@
+// package declaration
+package main
+
+import "fmt"
+
+// Imports
+
+// Main function
+func main() {
+	fmt.Println("Hello, World!")
+}
+
+// HandleWrongAnswer function
+
+// HandleRightAnswer function
